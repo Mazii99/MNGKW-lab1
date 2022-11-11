82ea3bb4b098bb8c763c3e1d921284aa81d4291a
Mazii <damiantatarczuk99@gmail.com>
2022-11-11 18:57:32 +0100

    wersja finalna (obiektowa)

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
-import csv,re
-def getData(directory):
-    header =['no','vol', 'article number' ,'pages in range', 'publisher name', 'publisher location', 'publisher year']
-    data = []
-    with open(directory, 'r', encoding="utf-8") as file:
-        csvreader = csv.reader(file)
-        for row in csvreader:
-            print(row[0])
-            regexHandlerC1 = re.search(r'((?P<plocation>\w+([-]|([,]\s)|\s)?(\w+)?))\s(:|;)\s(?P<pname>\w+.*(('
-                                       r'\s\w+)|(\s.\s))*\s?)(,|\\)\s?(cop\.\s)?(?P<pyear>\d\d\d\d)?',row[0])
-            regexHandlerC2 = re.search(r'(vol\.\s(?P<vol>\d+))?(,?\s?)((iss|nO|No|no|nr)(\.?)\s(?P<num>\d+))?\s?((['
-                                       r's]|[S])\.\s(?P<pagerange>\d+[-]\d+))?\s(art\.(?P<art>\w\d+))?',row[1])
-            print(regexHandlerC2.group('num'))
-            data.append([regexHandlerC2.group('num'),regexHandlerC2.group('vol'),regexHandlerC2.group('art'),regexHandlerC2.group('pagerange'),regexHandlerC1.group('pname'),regexHandlerC1.group('plocation'),regexHandlerC1.group('pyear')])
-            print(regexHandlerC2.group('vol'))
-            #vol.append(regexHandlerC2.group('vol'))
-            print(regexHandlerC2.group('pagerange'))
-            #pageRange.append(regexHandlerC2.group('pagerange'))
-            print(regexHandlerC2.group('art'))
-            #art.append(regexHandlerC2.group('art'))
-            print(regexHandlerC1.group('pname'))
-            #publisherName.append(regexHandlerC2.group('pname'))
-            print(regexHandlerC1.group('plocation'))
-            #publisherLocation.append(regexHandlerC2.group('plocation'))
-            print(regexHandlerC1.group('pyear'))
-            #publisherYear.append(regexHandlerC2.group('pyear'))
-            print('---------')
-    file.close()
-    writeData(header, data)
-def writeData(header, data):
-    file = open('analyzed.csv','a+', encoding="utf-8", newline='')
-    with file:
-        csvwriter = csv.DictWriter(file, fieldnames = header)
-        csvwriter.writeheader()
-        csvwriter = csv.writer(file)
-        csvwriter.writerows(data)
-def test():
-    txt = '2019, vol. 9, iss. 1 s. 132-136 - tab. - Summ. - Bibliogr. 34 poz.'
-    #matcher = re.compile(r'(vol\.\s(?P<vol>\d+))?')
-    print(re.search(r'(vol\.\s(?P<vol>\d+))?',txt).groups())
-#getData("G:\MNGKW\details.csv")
+#Ekstrakcja danych z pliku csv przy użyciu wyrażeń regularnych
+#Damian Tatarczuk WCY19KA1S0
+import csv,re, os
+class Extraction():
+    def __init__(self, directory):
+        self.directory = directory
+        self.data = []
+        self.regexForC1 = (r'((?P<plocation>\w+([-]|([,]\s)|\s)?(\w+)?))\s(:|;)\s(?P<pname>\w+.*(('
+                               r'\s\w+)|(\s.\s))*\s?)(,|\\)\s?(cop\.\s)?(?P<pyear>\d\d\d\d)?')
+        self.regexForC2 = (r'(\d\d\d\d,\s)?(t\.\s\d\d,\s)?(vol\.\s(?P<vol>\d+))?(,?\s?)((iss|nO|No|no|nr|num)(\.?)\s(?P<num>\d+))?\s?((['
+        r's]|[S])\.\s(?P<pagerange>\d+[-]\d+))?\s(((nr\sart\.)|(Article\sID))\s(?P<art>\w\d+))?')
+        self.header = ['no', 'vol', 'article number', 'pages in range', 'publisher name', 'publisher location', 'publisher year']
+    def runExtraction(self):
+        self.openReadFile()
+        self.getData()
+        self.extractData()
+        self.openWriteFile()
+        self.writeData()
+        self.readFile.close()
+        self.writeFile.close()
+    def openReadFile(self):
+        self.readFile = open(self.directory, 'r', encoding="utf-8")
 
-test()
+    def openWriteFile(self):
+        if os.path.exists('analyzed.csv'):
+            os.remove('analyzed.csv')
+        self.writeFile = open('analyzed.csv', 'a+', encoding="utf-8", newline='')
+    def getData(self):
+        self.csvreader = csv.reader(self.readFile)
+    def extractData(self):
+        for row in self.csvreader:
+            regexHandlerC1 = re.search(self.regexForC1, row[0])
+            regexHandlerC2 = re.search(self.regexForC2, row[1])
+            num = regexHandlerC2.group('num')
+            vol = regexHandlerC2.group('vol')
+            art = regexHandlerC2.group('art')
+            pagerange = regexHandlerC2.group('pagerange')
+            pname = regexHandlerC1.group('pname')
+            plocation = regexHandlerC1.group('plocation')
+            pyear = regexHandlerC1.group('pyear')
+            self.data.append([num, vol, art, pagerange, pname, plocation, pyear])
+
+    def writeData(self):
+        with self.writeFile:
+            csvwriter = csv.DictWriter(self.writeFile, fieldnames=self.header)
+            csvwriter.writeheader()
+            csvwriter = csv.writer(self.writeFile)
+            csvwriter.writerows(self.data)
+
+extraction = Extraction("G:\MNGKW\details.csv") #Należy podać lokalizację pliku csv do ekstrakcji danych
+extraction.runExtraction()
