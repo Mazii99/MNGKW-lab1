9a23063468a1ce0da6a3bc8ff13cc7a5d883710f
Mazii <damiantatarczuk99@gmail.com>
2022-11-19 13:29:22 +0100

    Poprawa programu, zmiana podejścia z zapisu do pliku na ekstrakcję danych metodami w klasie Extractor, testy tych metod w pliku test_main w oparciu o plik źródłowy details.csv

--- main.py
+++ main.py
@@ -1,55 +1,59 @@
 #Ekstrakcja danych z pliku csv przy użyciu wyrażeń regularnych
 #Damian Tatarczuk WCY19KA1S0
-import csv,re, os
-class Extraction():
-    def __init__(self, directory): #kreator obiektu
-        self.directory = directory # przypisanie ścieżki do pliku wejściowego z parametru kreatora do zmiennej
-        self.data = [] # lista do przechowywania danych z ekstrakcji
+import csv, re, os
+
+class Extractor():
+    def __init__(self, Column1= None, Column2 = None):
+        self.data = []  # lista do przechowywania danych z ekstrakcji
         self.regexForC1 = (r'((?P<plocation>\w+([-]|([,]\s)|\s)?(\w+)?))\s(:|;)\s(?P<pname>\w+.*(('
-                               r'\s\w+)|(\s.\s))*\s?)(,|\\)\s?(cop\.\s)?(?P<pyear>\d\d\d\d)?') # wyrażenie regularne dla 1 kolumny
-        self.regexForC2 = (r'(\d\d\d\d,\s)?(t\.\s\d\d,\s)?(vol\.\s(?P<vol>\d+))?(,?\s?)((iss|nO|No|no|nr|num)(\.?)\s(?P<num>\d+))?\s?((['
-        r's]|[S])\.\s(?P<pagerange>\d+[-]\d+))?\s(((nr\sart\.)|(Article\sID))\s(?P<art>\w\d+))?') # wyrażenie regularne dla 2 kolumny
-        self.header = ['no', 'vol', 'article number', 'pages in range', 'publisher name', 'publisher location', 'publisher year'] # nagłówek do pliku wyjściowego
-    def runExtraction(self): # metoda przeprowadzająca ekstrakcję fraz z pliku csv
-        self.openReadFile()
-        self.getData()
-        self.extractData()
-        self.openWriteFile()
-        self.writeData()
-        self.readFile.close()
-        self.writeFile.close()
-    def openReadFile(self): # metoda do otwierania pliku wejściowego
-        self.readFile = open(self.directory, 'r', encoding="utf-8")
-
-    def openWriteFile(self): # metoda do tworzenia i otwarcia pliku wyjściowego
-        if os.path.exists('analyzed.csv'):
-            os.remove('analyzed.csv')
-        self.writeFile = open('analyzed.csv', 'a+', encoding="utf-8", newline='')
-    def getData(self): # metoda do odczytu pliku wejściowego w formacie csv
-        self.csvreader = csv.reader(self.readFile)
-    def extractData(self): # metoda do ekstrakcji danych z pliku
-        for row in self.csvreader:
-            #przeszukanie pierwszych dwóch kolumn wiersza przy użyciu wyrażeń regularnych
-            regexHandlerC1 = re.search(self.regexForC1, row[0])
-            regexHandlerC2 = re.search(self.regexForC2, row[1])
-            #przypisanie danych z obiektu klasy re.search przy użyciu metody group do zmiennych
-            num = regexHandlerC2.group('num')
-            vol = regexHandlerC2.group('vol')
-            art = regexHandlerC2.group('art')
-            pagerange = regexHandlerC2.group('pagerange')
-            pname = regexHandlerC1.group('pname')
-            plocation = regexHandlerC1.group('plocation')
-            pyear = regexHandlerC1.group('pyear')
-            #wpisanie danych do listy
-            self.data.append([num, vol, art, pagerange, pname, plocation, pyear])
-
-    def writeData(self): #metoda do zapisu danych do pliku wyjściowego
-        with self.writeFile:
-            csvwriter = csv.DictWriter(self.writeFile, fieldnames=self.header)
-            csvwriter.writeheader()
-            csvwriter = csv.writer(self.writeFile)
-            csvwriter.writerows(self.data)
-#utworzenie obiektu
-extraction = Extraction("G:\MNGKW\details.csv") #Należy podać lokalizację pliku csv do ekstrakcji danych
-#uruchomienie metody z obiektu klasy Extraction do przeprowadzenia ekstrakcji danych
-extraction.runExtraction()
+                           r'\s\w+)|(\s.\s))*\s?)(,|\\)\s?(cop\.\s)?(?P<pyear>\d\d\d\d)?')  # wyrażenie regularne dla 1 kolumny
+        self.regexForC2 = (
+            r'(\d\d\d\d,\s)?(t\.\s\d\d,\s)?(vol\.\s(?P<vol>\d+))?(,?\s?)((iss|nO|No|no|nr|num)(\.?)\s(?P<num>\d+))?\s?((['
+            r's]|[S])\.\s(?P<pagerange>\d+[-]\d+))?\s(((nr\sart\.)|(Article\sID))\s(?P<art>\w\d+))?')  # wyrażenie regularne dla 2 kolumny
+        self.Column1 = Column1
+        self.Column2 = Column2
+    def extractNum(self, row): #Metoda zwracająca dane z ekstrakcji wartości num
+        regexHandlerC2 = re.search(self.regexForC2, row)
+        if regexHandlerC2.group('num') is None:
+            return ''
+        else:
+            return regexHandlerC2.group('num')
+    def extractVol(self, row): #Metoda zwracająca dane z ekstrakcji wartości vol
+        regexHandlerC2 = re.search(self.regexForC2, row)
+        if regexHandlerC2.group('vol') is None:
+            return ''
+        else:
+            return regexHandlerC2.group('vol')
+    def extractArt(self, row): #Metoda zwracająca dane z ekstrakcji wartości art
+        regexHandlerC2 = re.search(self.regexForC2, row)
+        if regexHandlerC2.group('art') is None:
+            return ''
+        else:
+            return regexHandlerC2.group('art')
+    def extractPageRange(self, row): #Metoda zwracająca dane z ekstrakcji wartości pages in range
+        regexHandlerC2 = re.search(self.regexForC2, row)
+        if regexHandlerC2.group('pagerange') is None:
+            return ''
+        else:
+            return regexHandlerC2.group('pagerange')
+    def extractName(self, row): #Metoda zwracająca dane z ekstrakcji wartości publisher name
+        regexHandlerC1 = re.search(self.regexForC1, row)
+        if regexHandlerC1.group('pname')  is None:
+            return ''
+        else:
+            return regexHandlerC1.group('pname')
+    def extractLocation(self, row): #Metoda zwracająca dane z ekstrakcji wartości publisher location
+        regexHandlerC1 = re.search(self.regexForC1, row)
+        if regexHandlerC1.group('plocation') is None:
+            return ''
+        else:
+            return regexHandlerC1.group('plocation')
+    def extractYear(self, row): #Metoda zwracająca dane z ekstrakcji wartości publisher year
+        regexHandlerC1 = re.search(self.regexForC1, row)
+        if regexHandlerC1.group('pyear') is None:
+            return ''
+        else:
+            return regexHandlerC1.group('pyear')
+    def extractData(self): #Metoda która wywołuje wszystkie metody do ekstrakcji w celu zapisania danych w liście data
+        for rowC1, rowC2 in self.Column1, self.Column2 :
+            self.data.append([self.extractNum(rowC2), self.extractVol(rowC2), self.extractArt(rowC2), self.extractPageRange(rowC2), self.extractName(rowC1), self.extractLocation(rowC1), self.extractYear(rowC1)])
--- test_main.py
+++ test_main.py
@@ -1,54 +1,41 @@
 from unittest import TestCase
-from main import Extraction
-
+from main import Extractor
 import csv
-class TestExtraction(TestCase):
-    def setUp(self):
-        self.extraction = Extraction("G:\MNGKW\details.csv")
-        self.csvreader = csv.reader(open(self.extraction.directory, 'r', encoding="utf-8"))
-        self.extraction.openReadFile()
-        self.extraction.getData()
-        self.extraction.extractData()
-    def test_extractNum(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader): #test dla kolumny num
-             with self.subTest():
-                if row[0] is None:
-                     row[0] = ''
-                self.assertEqual(row[0], row2[2])
 
-    def test_extractVol(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny vol
-             with self.subTest():
-                 if row[1] is None:
-                     row[1] = ''
-                 self.assertEqual(row[1], row2[3])
-    def test_extractArt(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny art
-             with self.subTest():
-                 if row[2] is None:
-                     row[2] = ''
-                 self.assertEqual(row[2], row2[4])
-    def test_extractPageRange(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny page range
-             with self.subTest():
-                 if row[3] is None:
-                     row[3] = ''
-                 self.assertEqual(row[3], row2[5])
-    def test_extractPublisherName(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny publisher name
-             with self.subTest():
-                 if row[4] is None:
-                     row[4] = ''
-                 self.assertEqual(row[4], row2[7])
-    def test_extractPublisherLocation(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny publisher location
-             with self.subTest():
-                 if row[5] is None:
-                     row[5] = ''
-                 self.assertEqual(row[5], row2[8])
-    def test_extractPublisherYear(self):
-         for (row, row2) in zip(self.extraction.data, self.csvreader):  # test dla kolumny publisher year
-             with self.subTest():
-                 if row[6] is None:
-                     row[6] = ''
-                 self.assertEqual(row[6], row2[9])
+class TestExtractor(TestCase):
+    def setUp(self):
+        self.csvreader = csv.reader(open("details.csv", 'r', encoding="utf-8"))
+        self.Extractor = Extractor()
+    def test_extract_num(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractNum(row[1]),row[2])
+    def test_extract_vol(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractVol(row[1]), row[3])
+
+    def test_extract_art(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractArt(row[1]), row[4])
+
+    def test_extract_page_range(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractPageRange(row[1]), row[5])
+
+    def test_extract_name(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractName(row[0]), row[7])
+
+    def test_extract_location(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractLocation(row[0]), row[8])
+
+    def test_extract_year(self):
+        for row in self.csvreader:
+            with self.subTest():
+                self.assertEqual(self.Extractor.extractYear(row[0]), row[9])
